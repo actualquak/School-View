67baae72050bd2dfa9dea78c6e3c25c1da404e7d
actualquak <80514422+actualquak@users.noreply.github.com>
2021-06-10 09:53:25 +1000

    Add files via upload

--- json/base.js
+++ json/base.js
@@ -7,39 +7,9 @@ base = `{
     },
 
     "scenes": {
-        "room1": {
-            "hfov": 110,
-            "pitch": -3,
-            "yaw": 117,
-            "type": "equirectangular",
-            "panorama": "https://actualquak.github.io/School-View/room1.jpg",
-            "hotSpots": [
-                {
-                    "pitch": -2.1,
-                    "yaw": 132.9,
-                    "type": "scene",
-                    "text": "Airport",
-                    "sceneId": "room2"
-                }
-            ]
-        },
+        "room1": {},
 
-        "room2": {
-            "hfov": 110,
-            "yaw": 5,
-            "type": "equirectangular",
-            "panorama": "https://actualquak.github.io/School-View/360_1.jpg",
-            "hotSpots": [
-                {
-                    "pitch": -0.6,
-                    "yaw": 37.1,
-                    "type": "scene",
-                    "text": "Room",
-                    "sceneId": "room1",
-                    "targetYaw": -23,
-                    "targetPitch": 2
-                }
-            ]
-        }
+        "room2": {}
     }
 }`;
+json = JSON.parse(base)
