a01841a5d5cfb7a7f58238fddcd9c4ca11365c53
actualquak <80514422+actualquak@users.noreply.github.com>
2021-06-10 09:30:04 +1000

    Add files via upload

--- tour.js
+++ tour.js
@@ -1,48 +1,9 @@
-data = `{   
-    "default": {
-    	"hotSpotDebug" : true,
-        "firstScene": "room1",
-        "sceneFadeDuration": 1000,
-	"autoLoad" : true
-    },
-
-    "scenes": {
-        "room1": {
-            "hfov": 110,
-            "pitch": -3,
-            "yaw": 117,
-            "type": "equirectangular",
-            "panorama": "https://actualquak.github.io/School-View/room1.jpg",
-            "hotSpots": [
-                {
-                    "pitch": -2.1,
-                    "yaw": 132.9,
-                    "type": "scene",
-                    "text": "Airport",
-                    "sceneId": "room2"
-                }
-            ]
-        },
-
-        "room2": {
-            "hfov": 110,
-            "yaw": 5,
-            "type": "equirectangular",
-            "panorama": "https://actualquak.github.io/School-View/360_1.jpg",
-            "hotSpots": [
-                {
-                    "pitch": -0.6,
-                    "yaw": 37.1,
-                    "type": "scene",
-                    "text": "Room",
-                    "sceneId": "room1",
-                    "targetYaw": -23,
-                    "targetPitch": 2
-                }
-            ]
-        }
-    }
-}`;
+$.ajax({
+	url: 'https://actualquak.github.io/json/base.json',
+	success: function (jsonData) {
+		data = jsonData;
+	}
+});
 
 function getUrlVars() {
     var vars = {};
