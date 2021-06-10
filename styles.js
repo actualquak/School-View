13fee880a6b0f777d26a5a905aa3b3ec72477311
frlegg <84002781+frlegg@users.noreply.github.com>
2021-06-10 19:31:51 +1000

    Update styles.js

--- styles.js
+++ styles.js
@@ -7,16 +7,6 @@ function startPageUp() {
       console.log("startPage = 0");
       noStartPage();
   }
-  
-  function informationComesUp() {
-    var targetElement = document.getElementById("information");
-    targetElement.className = "comeUp";
-  }
-  
-  function informationGoesDown() {
-    var targetElement = document.getElementById("information");
-    targetElement.className = "goDown";
-  }
 
 function noStartPage() {
     var targetElement = document.getElementById("startPage");
