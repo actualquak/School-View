1d710bbc539d0266c485ba42ec8b4ad08673c531
frlegg <84002781+frlegg@users.noreply.github.com>
2021-06-11 16:41:01 +1000

    Update tour.js

--- tour.js
+++ tour.js
@@ -22,25 +22,3 @@ console.log(json.default.firstScene);
 
 startPage = getUrlParam("startPage", 1);
 
-// Custom pannellum controls
-document.getElementById('pan-up').addEventListener('click', function(e) {
-    viewer.setPitch(viewer.getPitch() + 10);
-});
-document.getElementById('pan-down').addEventListener('click', function(e) {
-    viewer.setPitch(viewer.getPitch() - 10);
-});
-document.getElementById('pan-left').addEventListener('click', function(e) {
-    viewer.setYaw(viewer.getYaw() - 10);
-});
-document.getElementById('pan-right').addEventListener('click', function(e) {
-    viewer.setYaw(viewer.getYaw() + 10);
-});
-document.getElementById('zoom-in').addEventListener('click', function(e) {
-    viewer.setHfov(viewer.getHfov() - 10);
-});
-document.getElementById('zoom-out').addEventListener('click', function(e) {
-    viewer.setHfov(viewer.getHfov() + 10);
-});
-document.getElementById('fullscreen').addEventListener('click', function(e) {
-    viewer.toggleFullscreen();
-});
