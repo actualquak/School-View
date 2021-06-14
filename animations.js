71071892a70f5b4fe5439692ea2dbb29faae2913
frlegg <84002781+frlegg@users.noreply.github.com>
2021-06-14 11:41:55 +1000

    Update animations.js

--- animations.js
+++ animations.js
@@ -1,14 +1,18 @@
+startPage = getUrlParam("startPage", 1);
+startPageElement = `<div id="alignStart"><div id="verticalAlign"><h1>Explore New Town High</h1><div id="padding"><button id="startButton" onclick="startPageUp();">Start Exploring</button></div> </div></div>`;
+
+generateStartPage();
+
 function startPageUp() {
     var targetElement = document.getElementById("startPage");
     targetElement.className = "startPageComeUp";
   }
 
-startPage = getUrlParam("startPage", 1);
-startPageElement = `<div id="alignStart"><div id="verticalAlign"><h1>Explore New Town High</h1><div id="padding"><button id="startButton" onclick="startPageUp();">Start Exploring</button></div> </div></div>`;
-
-if (startPage != 0) {
+function generateStartPage() {
+  if (startPage != 0) {
     $("#startPage").html(startPageElement);
-  }
+  }   
+}    
 
 function openNav() {
   document.getElementById("mySidenav").style.width = "100vw";
