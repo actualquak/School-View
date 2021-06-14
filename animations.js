dac5947424dc1c743e810381e54ce2e42a8f91ff
frlegg <84002781+frlegg@users.noreply.github.com>
2021-06-14 11:33:37 +1000

    Update animations.js

--- animations.js
+++ animations.js
@@ -7,8 +7,7 @@ startPage = getUrlParam("startPage");
 startPageElement = `<div id="alignStart"><div id="verticalAlign"><h1>Explore New Town High</h1><div id="padding"><button id="startButton" onclick="startPageUp();">Start Exploring</button></div> </div></div>`;
 
 if (startPage != 0) {
-    var targetElement = document.getElementById("startPage"); 
-    targetElement.innerHTML = startPageElement;
+    $("#startPage").html(startPageElement);
   }
 
 function openNav() {
