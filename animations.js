971b3d8fa16c0587b2c7dedb73d3b0fca971bfb3
frlegg <84002781+frlegg@users.noreply.github.com>
2021-06-14 11:38:43 +1000

    Update animations.js

--- animations.js
+++ animations.js
@@ -3,7 +3,7 @@ function startPageUp() {
     targetElement.className = "startPageComeUp";
   }
 
-startPage = getUrlParam("startPage");
+startPage = getUrlParam("startPage", 1);
 startPageElement = `<div id="alignStart"><div id="verticalAlign"><h1>Explore New Town High</h1><div id="padding"><button id="startButton" onclick="startPageUp();">Start Exploring</button></div> </div></div>`;
 
 if (startPage != 0) {
