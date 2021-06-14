0ed1300dac43a2caf993f1dcc721947200c0e189
frlegg <84002781+frlegg@users.noreply.github.com>
2021-06-14 10:57:42 +1000

    Update animations.js

--- animations.js
+++ animations.js
@@ -5,12 +5,14 @@ function startPageUp() {
 
 startPage = getUrlParam("startPage");
 console.log(startPage);
+instantComeUp = "instantComeUp";
+console.log(instantComeUp);
 
 if (startPage == 0) {
     console.log(startPage);
     var targetElement = document.getElementById("startPage");
     console.log(targetElement);
-    targetElement.className = "instantComeUp";
+    targetElement.className = instantComeUp;
     console.log(targetElement.className);
   }
 
