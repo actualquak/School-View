0199aff59d0984bc93286b921bda39d217483e98
actualquak <80514422+actualquak@users.noreply.github.com>
2021-06-17 09:48:54 +1000

    Add files via upload

--- json/base.js
+++ json/base.js
@@ -27,6 +27,11 @@ $.when(
 		dataType: "json",
 		url: "https://actualquak.github.io/School-View/json/Down-CenNE.json",
 		success: function(data) {json.scenes.DownCenNE = data;}
+	}),
+	$.ajax({
+		dataType: "json",
+		url: "https://actualquak.github.io/School-View/json/Down-Foyer.json",
+		success: function(data) {json.scenes.DownFoyer = data;}
 	})
 ).then(function() {
 	pannellum.viewer('panorama', json); console.log(link);
