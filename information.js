cb86b5d9713a4db3a49d61023a02b6a4d2302d69
frlegg <84002781+frlegg@users.noreply.github.com>
2021-06-17 18:02:37 +1000

    Update information.js

--- information.js
+++ information.js
@@ -1,3 +1,11 @@
+function getUrlVars() {
+    var vars = {};
+    var parts = window.location.href.replace(/[?&]+([^=&]+)=([^&]*)/gi, function(m,key,value) {
+        vars[key] = value;
+    });
+    return vars;
+};
+
 function getUrlParam(parameter, defaultvalue){
     var urlparameter = defaultvalue;
     if(window.location.href.indexOf(parameter) > -1){
