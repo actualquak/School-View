77fb9b2cdcfc7cb9c4018f1262378236f59c2578
frlegg <84002781+frlegg@users.noreply.github.com>
2021-06-18 17:02:01 +1000

    Create navigation.js

---
+++ navigation.js
@@ -0,0 +1,13 @@
+function togglePannellum() {
+  if (informationDisplay == true) { 
+    $("#information").addClass("comeUp");
+    window.setTimeout(function(){ $("#information").removeClass("comeUp"); }, 2000);
+    informationDisplay = false; 
+    width = window.innerWidth - 250; 
+    $("#panorama").css("width", width+"px");
+  } else { 
+    $("#information").addClass("goDown"); 
+    window.setTimeout(function(){ $("#information").removeClass("comeUp"); }, 2000);
+    informationDisplay = true; 
+  } 
+}
