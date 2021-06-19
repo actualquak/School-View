a698541fee896ffdee9c33cbae8c5b58659b9d51
frlegg <84002781+frlegg@users.noreply.github.com>
2021-06-19 11:27:29 +1000

    Update navigation.js

--- navigation.js
+++ navigation.js
@@ -1,7 +1,26 @@
 function changeSection(to) {
-  $(currentSection).removeClass("goDownFromTop");
-  $(currentSection).addClass("comeUpFromMiddle");
-  $(to).removeClass("goDownFromMiddle");
-  $(to).addClass("comeUpFromBottom");
-  currentSection = to;
+  // working out positions
+  if (currentSection == "#information") { currentPosition = 0; }
+  else if (currentSection == "#panorama") { currentPosition = 1; }
+  else { currentPosition = 2; }
+  
+  if (to == "#information") { newPosition = 0; }
+  else if (to == "#panorama") { newPosition = 1; }
+  else { newPosition = 2; }
+  
+  // moving sections around
+  if (currentSection != to ) { // Don't move section if already selected
+    if (currentPosition < newPosition) { 
+      // Removing current section from view
+      $(currentSection).addClass("comeUpFromMiddle");
+      // Moving new section into view
+      $(to).addClass("comeUpFromBottom");
+    } else {
+      // Removing current section from view
+      $(currentSection).addClass("goDownFromMiddle");
+      // Moving new section into view
+      $(to).addClass("goDownFromTop");
+    }  
+    currentSection = to;
+  }
 }
