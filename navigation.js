c176c1def964c6b4ef2ba980b65bc7ece1bd0650
frlegg <84002781+frlegg@users.noreply.github.com>
2021-06-19 10:35:01 +1000

    Update navigation.js

--- navigation.js
+++ navigation.js
@@ -1,13 +1,13 @@
 function togglePannellum() {
   if (informationDisplay == true) { 
-    $("#information").removeClass("goDown");
-    $("#information").addClass("comeUp");
+    $("#information").removeClass("goDownFromTop");
+    $("#information").addClass("comeUpFromMiddle");
     informationDisplay = false; 
     width = window.innerWidth - 250; 
     $("#panorama").css("width", width+"px");
   } else { 
-    $("#information").removeClass("comeUp");
-    $("#information").addClass("goDown"); 
+    $("#information").removeClass("comeUpFromMiddle");
+    $("#information").addClass("goDownFromTop"); 
     window.setTimeout(function(){  }, 2000);
     informationDisplay = true; 
   } 
