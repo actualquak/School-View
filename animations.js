874a4e3ffaf5bb6dc878dc66d1f28c386924d98e
frlegg <84002781+frlegg@users.noreply.github.com>
2021-06-20 11:44:41 +1000

    Add files via upload

---
+++ animations.js
@@ -0,0 +1,34 @@
+function getUrlVars() {
+    var vars = {};
+    var parts = window.location.href.replace(/[?&]+([^=&]+)=([^&]*)/gi, function(m,key,value) {
+        vars[key] = value;
+    });
+    return vars;
+};
+
+function getUrlParam(parameter, defaultvalue){
+    var urlparameter = defaultvalue;
+    if(window.location.href.indexOf(parameter) > -1){
+        urlparameter = getUrlVars()[parameter];
+        }
+    return urlparameter;
+};
+
+startPage = getUrlParam("startPage", 1);
+
+if (startPage == 0) { 
+    currentSection = "#information";
+    $("#startPage").hide(); 
+    openNav(); 
+}
+
+function startPageUp() {
+    $("#startPage").addClass("comeUpFromMiddle");
+    currentSection = "#information";
+    window.setTimeout(function(){ openNav("#main") }, 2000);
+  }
+
+function openNav(element) {
+  $("#mySidenav").css("width", "250px");
+  $(element).css("marginLeft", "250px");
+}
