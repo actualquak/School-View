779c59ecce3ffcf9db7a9d0995ce9145dfdd0a2c
frlegg <84002781+frlegg@users.noreply.github.com>
2021-06-20 12:55:22 +1000

    Update navigation.js

--- navigation.js
+++ navigation.js
@@ -1,29 +1,3 @@
-function changeSection(element) {
-  oldPos = getElementPosition(currentSection);
-  newPos = getElementPosition(element);
-
-  if (oldPos < newPos) {
-    removeAllClasses(currentSection)
-    $(currentSection).addClass("comeUpFromMiddle")
-    removeAllClasses(element)
-    $(element).addClass("comeUpFromBottom")
-  } else {
-    removeAllClasses(currentSection)
-    $(currentSection).addClass("goDownFromTop")
-    removeAllClasses(element)
-    $(element).addClass("goDownFromMiddle")
-  }  
-  
-  currentSection = element
-}
-
-function getElementPosition(element) {
-  if (element == "#information") { pos = 0 }
-  else if (element == "#panoramaPage") { pos = 1 }
-  else { pos = 2 }
-  return pos;
-}   
-
 function removeAllClasses(element) {
   $(element).removeClass("comeUpFromMiddle");
   $(element).removeClass("comeUpFromBottom");
