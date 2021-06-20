3b314415dbd7340809b7678cac07be3e8e69514d
frlegg <84002781+frlegg@users.noreply.github.com>
2021-06-20 12:11:44 +1000

    Update navigation.js

--- navigation.js
+++ navigation.js
@@ -1,1 +1,19 @@
+function changeSection(element) {
+  oldPos = getElementPosition(currentSection);
+  newPos = getElementPosition(element);
+  
+  if (oldPos < newPos) {
+    $(currentSection).addClass("comeUpFromMiddle")
+    $(element).addClass("comeUpFromBottom")
+  } else {
+    $(currentSection).addClass("goDownFromMiddle")
+    $(element).addClass("goDownFromTop")
+  }  
+}
 
+function getElementPosition(element) {
+  if (element == "#information") { pos = 0 }
+  else if (element == "#panoramaPage") { pos = 1 }
+  else { pos = 2 }
+  return pos;
+}  
