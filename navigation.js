83ce5b7faff7da2041c268b1df2969c22eb6cabd
frlegg <84002781+frlegg@users.noreply.github.com>
2021-06-20 13:42:07 +1000

    Update navigation.js

--- navigation.js
+++ navigation.js
@@ -1,3 +1,14 @@
+function move(element, direction, from) {
+  removeAllClasses(element);
+  if (direction == "up") {
+    if (from == "middle") { $(element).addClass("comeUpFromMiddle"); } 
+    else { $(element).addClass("comeUpFromBottom"); }
+  } else {
+    if (from == "middle") { $(element).addClass("goDownFromMiddle"); } 
+    else { $(element).addClass("goDownFromTop"); }
+  }  
+}  
+
 function removeAllClasses(element) {
   $(element).removeClass("comeUpFromMiddle");
   $(element).removeClass("comeUpFromBottom");
