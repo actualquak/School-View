0cae5d0af12e759130d866c7533ce54adf694a53
frlegg <84002781+frlegg@users.noreply.github.com>
2021-06-22 18:56:50 +1000

    Update animations.js

--- animations.js
+++ animations.js
@@ -1,3 +1,5 @@
+// Get URL parameters
+
 function getUrlVars() {
     var vars = {};
     var parts = window.location.href.replace(/[?&]+([^=&]+)=([^&]*)/gi, function(m,key,value) {
@@ -16,6 +18,8 @@ function getUrlParam(parameter, defaultvalue){
 
 startPage = getUrlParam("startPage", 1);
 
+// Act on URL parameters
+
 if (startPage == 0) {
     currentSection = "#information";
     $("#startPage").hide(); 
@@ -32,3 +36,32 @@ function openNav() {
     $("#nav").show();
     $("#nav").addClass("comeOut"); 
 }
+
+// Button Ripple Effect
+
+function createRipple(event) {
+  const button = event.currentTarget;
+
+  const circle = document.createElement("span");
+  const diameter = Math.max(button.clientWidth, button.clientHeight);
+  const radius = diameter / 2;
+
+  circle.style.width = circle.style.height = `${diameter}px`;
+  circle.style.left = `${event.clientX - button.offsetLeft - radius}px`;
+  circle.style.top = `${event.clientY - button.offsetTop - radius}px`;
+  circle.classList.add("ripple");
+
+  const ripple = button.getElementsByClassName("ripple")[0];
+
+  if (ripple) {
+    ripple.remove();
+  }
+
+  button.appendChild(circle);
+}
+
+const buttons = document.getElementsByTagName("button");
+for (const button of buttons) {
+  button.addEventListener("click", createRipple);
+}
+
