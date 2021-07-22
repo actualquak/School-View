bde0320b0cd629f648b6422286d918b69c2d1fc2
frlegg <84002781+frlegg@users.noreply.github.com>
2021-07-22 17:05:51 +1000

    Update modalGeneration.js

--- modalGeneration.js
+++ modalGeneration.js
@@ -47,8 +47,8 @@ $("#panoramaPage").append(modal("map", `
     </tr>
   </table>
   <div id="map-spots">
-  	<svg height="100" width="100">
-  		<circle cx="50" cy="50" r="40" stroke="black" stroke-width="3" fill="red" />
+  	<svg>
+  		<circle cx="50" cy="50" r="5" stroke="black" stroke-width="3" fill="red" />
 	</svg>
   </div>
 `));
